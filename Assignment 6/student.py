ead194c5c0d23a88248d46f5cef07028c8ca63c1
Slavitt <112398002+Slavitt@users.noreply.github.com>
2022-11-11 10:51:57 -0500

    ass 6

--- Assignment 6/student.py
+++ Assignment 6/student.py
@@ -23,16 +23,44 @@ create a function because this college is a wacky one- every day they generate a
 4. if not, print "Loser!"
 (disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want to see that you're generating and comparing properly)
 '''
-
+import random
 class student:
-    
-    
-    
-    
-    
-    
+    def __init__(self, name, id, year, major, gpa):
+        self.name = name
+        self.id = id
+        self.year = year
+        self.major = major
+        self.gpa = gpa
+
+    def honorsEligible(self):
+       # if self.gpa.isdigit():
+        #newGPA = self.gpa
+        if self.gpa > 3.5:
+            print("True")
+            return True
+        else:
+            print("False")
+            return False
+
+    def wackFunction(self):
+        randID = random.randint(100, 999)
+        if self.id == randID:
+            print("Winner! " + self.name + " gets free lunch!")
+        else:
+            print("You get nothing! You LOSE! Good DAY, sir!")
+
 def main():
     #create three students and check if they get free lunch and if they qualify for honors
-    
-    
+    sam = student("Samuel", 420, "freshman", "cs", 4.0)
+    other1 = student("Melissa", 690, "grad i think", "data science or something idk", 4.0)
+    other2 = student("placeholder", 123, "sophomore", "gender studies", 2.9)
+
+    sam.honorsEligible()
+    sam.wackFunction()
+
+    other1.honorsEligible()
+    other1.wackFunction()
+
+    other2.honorsEligible()
+    other2.wackFunction()
 main()
